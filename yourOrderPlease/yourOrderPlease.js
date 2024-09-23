702ea35f955d991645accce4921bb26f01b1a4df
luishsilva <luizhrick@gmail.com>
2024-09-22 22:38:33 -0600

    Finish your order algo

---
+++ yourOrderPlease/yourOrderPlease.js
@@ -0,0 +1,29 @@
+/**
+ * DESCRIPTION:
+    Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word 
+    should have in the result.
+
+    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+    If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+    Examples
+        "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+        "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+        ""  -->  ""
+
+    Inputs = strings containing numbers
+    Output = strings ordered by the number they contain
+ */
+
+const order = (str) => {
+    if (!str) return '';
+
+    const orderedNumbers = str.replace(/\D+/g, '').split('').sort((a, b) => Number(a) - Number(b));
+    const strArr = str.split(' ');
+    const result = [];
+    orderedNumbers.map((value) => {
+        result.push(strArr.find((word) => word.includes((value))));
+    })
+    return result.join(' ');
+}
