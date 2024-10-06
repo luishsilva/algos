9e0b07c52927a58147243c825142eb223db1ab63
luishsilva <luizhrick@gmail.com>
2024-10-06 07:34:54 -0600

    Finish fibonaci algo

---
+++ Fibonacci/fibonacci.js
@@ -0,0 +1,15 @@
+/*
+Prompt: Write a recursive function that returns the n-th number in the Fibonacci sequence using a recursive approach. 
+Bonus: write a non-recursive, iterative approach.
+
+Input: a number
+Output: sum of the numbers using the fibinacci formula F(n)=F(n−1)+F(n−2)
+
+*/
+
+const fibonacci = (number) => {
+    if (number <= 1 ) return number;
+      // console.log(number)
+    return fibonacci(number -1) + fibonacci(number - 2)
+  }
+  console.log(fibonacci(2))
