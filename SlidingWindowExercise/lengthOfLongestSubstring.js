a850983b4012304c07f855e7e45b2e8ce84dafa4
luishsilva <luizhrick@gmail.com>
2024-11-18 22:40:28 -0700

    Solve one more exercise to practice sliding window practice

---
+++ SlidingWindowExercise/lengthOfLongestSubstring.js
@@ -0,0 +1,33 @@
+/* 
+Longest Substring Without Repeating Characters
+Problem: Given a string 𝑆
+S, find the length of the longest substring without repeating characters.
+Input: S = "abcabcbb"
+Output: Longest substring length = 3 ("abc") 
+*/
+
+const lengthOfLongestSubstring = (str) => {
+    let charSet = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < str.length; right++) {
+        
+        while (charSet.has(str[right])) {
+            // remove the left element in the collection
+            charSet.delete(str[left]);
+            // console.log(charSet)
+            left++;
+        }
+
+        charSet.add(str[right]);
+        // console.log(charSet)
+
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    return maxLength;
+}
+console.log(lengthOfLongestSubstring('abcabcbb')); // 3
+console.log(lengthOfLongestSubstring('aaeeiioouu')); // 2
+console.log(lengthOfLongestSubstring('aeiou')); // 5
+console.log(lengthOfLongestSubstring('aabccd')); // 3
