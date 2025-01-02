52db8e100a673b0394d582357c43cf37357bdd46
luishsilva <luizhrick@gmail.com>
2025-01-01 21:24:37 -0700

    Refactor twoSum leetcode algo

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -25,21 +25,19 @@
     -109 <= target <= 109
 */
 const twoSum = (nums, target) => {
-  if (nums.length === 2) {
-    return nums[0] + nums[1] === target ? [0, 1] : [];
-  }
+  const indexes = {};
 
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j <= nums.length; j++) {
-      if (nums[j] !== undefined && nums[i] + nums[j] === target) return [i, j];
+    const num = nums[i];
+    if (target - num in indexes) {
+      return [i, indexes[target - num]];
     }
+    indexes[num] = i;
   }
-
-  return [];
+  return indexes;
 };
 
 console.log(twoSum([0, 4, 3, 0], 0)); // [0,3]
-// console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
-// console.log(twoSum([3, 2, 4], 6)); // [1,2]
-// console.log(twoSum([3, 3], 6)); // [0,1]
-// console.log(twoSum([1, 2, 6], 0)); // [0]
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+console.log(twoSum([3, 2, 4], 6)); // [1,2]
+console.log(twoSum([3, 3], 6)); // [0,1]
